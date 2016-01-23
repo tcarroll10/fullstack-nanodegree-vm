72657c4b8777147de9332fe32d80f69cd25251cd
tcarroll10 <tcarroll10@gmail.com>
2016-01-22 20:14:45 -0500

    project changes

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
-
+# Author Tom Carroll
 import psycopg2
 
 
@@ -13,26 +13,44 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    pg = connect()
+    c = pg.cursor()
+    c.execute("DELETE FROM match;")
+    pg.commit()
+    pg.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    pg = connect()
+    c = pg.cursor()
+    c.execute("DELETE FROM player;")
+    pg.commit()
+    pg.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    pg = connect()
+    c = pg.cursor()
+    c.execute("Select count(*) from player;")
+    result = c.fetchone()
+    pg.close()
+    return int(result[0])
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    pg = connect()
+    c = pg.cursor()
+    c.execute("INSERT INTO player (name) values(%s);", (name,))
+    pg.commit()
+    pg.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,7 +65,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
+    pg = connect()
+    c = pg.cursor()
+    c.execute("select * from standings;")
+    result = c.fetchall();
+    pg.close()
+    return result
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,16 +79,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+    pg = connect()
+    c = pg.cursor()
+    c.execute("insert into match values (%s,%s);", (winner, loser))
+    pg.commit()
+    pg.close()
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -74,4 +102,8 @@ def swissPairings():
         name2: the second player's name
     """
 
-
+    result = playerStandings()
+    pairs = []
+    for i in xrange(0, len(result),2):
+        pairs.append((result[i][0], result[i][1], result[i+1][0], result[i+1][1]))
+    return pairs;
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -7,3 +7,18 @@
 -- these lines here.
 
 
+CREATE DATABASE tournament;
+\c tournament
+
+CREATE TABLE player(id serial,
+              name text,
+              PRIMARY KEY(id)
+            );
+
+CREATE TABLE match(winner_id integer,
+                    loser_id integer,
+                    FOREIGN KEY (winner_id) REFERENCES player(id),
+                    FOREIGN KEY (loser_id) REFERENCES player(id),
+                    PRIMARY KEY (winner_id,loser_id)
+                  );
+Create View Standings as SELECT id, name, (select count(*) from match where winner_id = id) as wins,(select count(*) from match where winner_id =id or loser_id = id) as matches from player order by wins desc;
